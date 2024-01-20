220358f2cfab527e4268f74f9fff46b794e178db
tjaskolka <63914143+tjaskolka@users.noreply.github.com>
2024-01-20 18:04:49 -0500

    built contact-list html

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactsComponent,
+    ContactDetailComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule,
